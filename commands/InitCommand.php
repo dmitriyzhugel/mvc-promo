f53e3487b9280d79756079e4726e2f6a9ce9f970
dmitriyzhugel <dzhugel@mail.ru>
2018-08-12 16:07:52 +0500

    init command

--- commands/InitCommand.php
+++ commands/InitCommand.php
@@ -7,6 +7,11 @@ class InitCommand {
 
     public function actionIndex(){
         echo '...run init command';
+        $begin_date = new DateTime('2015-06-01');
+        $cur_date = new DateTime();
+        // Заполним данные по каждому курьеру
+        for($i_courier=1;$i_courier<=10;$i_courier++){
+        }
     }
 
 }
